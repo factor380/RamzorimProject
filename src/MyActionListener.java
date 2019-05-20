bb4e312af217ff772275d8d0fcb9159f4f164959
factor380 <wer380@gmail.com>
2019-05-20 16:43:56 +0300

    acton

--- src/MyActionListener.java
+++ src/MyActionListener.java
@@ -2,23 +2,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JRadioButton;
 
-/*
- * Created on Tevet 5770 
- */
 
-/**
- * @author לויאן
- */
+
 
 
 public class MyActionListener implements ActionListener
 {
+	Event64 evButtonRegel_9_10,evButtonRegel_11_8_15_14;
+	public void init(Event64 evButtonRegel_9_10, Event64 evButtonRegel_11_8_15_14) {
+		this.evButtonRegel_9_10=evButtonRegel_9_10;
+		this.evButtonRegel_11_8_15_14=evButtonRegel_11_8_15_14;
+	}
 	public void actionPerformed(ActionEvent e) 
 	{
 		JRadioButton butt=(JRadioButton)e.getSource();
 		System.out.println(butt.getName());
-		//		butt.setEnabled(false);
-		//		butt.setSelected(false);
+		if(butt.getName().equals(9)||butt.getName().equals(10))
+		{
+			evButtonRegel_9_10.sendEvent();
+		}
+		else if(butt.getName().equals(11)||butt.getName().equals(8)||butt.getName().equals(15)||butt.getName().equals(14))
+		{
+			evButtonRegel_11_8_15_14.sendEvent();
+		}
+		
+		else if(butt.getName().equals(16))//לשבת
+		{}
+		
 	}
 
 }
